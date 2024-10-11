4ad4b3e3d8cfa0ee91394b802009af2c5d26ea3a
adrian.buda <adrian.buda.dev@gmail.com>
2024-10-11 14:13:20 +0300

    Add script to read System-Versioned table history

---
+++ Script to see history of System-Versioned table.sql
@@ -0,0 +1,17 @@
+DECLARE @addressId uniqueidentifier = '';
+
+WITH HistoryCTE AS (
+    SELECT
+        *,
+		Table_Name.ValidFrom,
+        LEAD(ValidFrom) OVER (PARTITION BY Id ORDER BY ValidFrom) AS NextSysStartTime
+    FROM dbo.Table_Name
+    FOR SYSTEM_TIME ALL
+    WHERE Id = @addressId
+)
+SELECT
+    *,
+    ISNULL(NextSysStartTime, '9999-12-31') AS SysEndTime
+FROM
+    HistoryCTE
+ORDER BY HistoryCTE.ValidFrom DESC;
